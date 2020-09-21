f3fb6ae8ca6676afecde7969ae4dc29f6614e84b
chris-greening <chris@christophergreening.com>
2020-09-21 19:56:25 -0400

    Created Post.from_shortcode classmethod and Post.__init__

--- post.py
+++ post.py
@@ -2,5 +2,12 @@ import requests
 from bs4 import BeautifulSoup 
 
 class Post: 
-    def __init__(self):
-        pass 
+    def __init__(self, url):
+        self.url = url
+         
+    @classmethod
+    def from_shortcode(cls, shortcode: str) -> Post:
+        """Return a Post given a shortcode"""
+        url = f'https://www.instagram.com/{shortcode}/'
+        return Post(url)
+    
