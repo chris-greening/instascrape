9d58ee054b190ec062a44e13b9aaf5e639f3dad3
chris-greening <chris@christophergreening.com>
2020-09-21 20:03:56 -0400

    Post.load_page_source created to get HTML from post url

--- post.py
+++ post.py
@@ -5,6 +5,11 @@ class Post:
     def __init__(self, url):
         self.url = url
          
+    def load_page_source(self):
+        """Load the static HTML into a BeautifulSoup object at the url"""
+        self.page_source = requests.get(self.url)
+        self.soup = BeautifulSoup(self.page_source)
+
     @classmethod
     def from_shortcode(cls, shortcode: str) -> Post:
         """Return a Post given a shortcode"""
