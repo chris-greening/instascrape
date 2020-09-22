cc1fddd4444a3ef7395dbf33f9fb162d10750d64
chris-greening <chris@christophergreening.com>
2020-09-22 00:06:34 -0400

    Renamed load_page_source method to load

--- profile.py
+++ profile.py
@@ -2,6 +2,17 @@ class Profile:
     def __init__(self, url):
         self.url = url
     
+    def load(self):
+        """Load the static HTML into a BeautifulSoup object at the url"""
+        self.page_source = requests.get(self.url).text
+        self.soup = BeautifulSoup(self.page_source, features='lxml')
+
+        self._scrape_soup()
+
+    def _scrape_soup(self):
+        """Scrape data from the soup"""
+        pass
+
     @classmethod 
     def from_username(cls, username):
         url = f'https://www.instagram.com/{username}/'
