5d5b753aba16b9479b4fc0c678235d7eab9ed7fe
chris-greening <chris@christophergreening.com>
2020-09-22 00:28:59 -0400

    Started adding in scraping of json data

--- profile.py
+++ profile.py
@@ -1,3 +1,11 @@
+import json
+
+import requests 
+from bs4 import BeautifulSoup
+
+#TODO: A lot of the code from this and Post can probably be consolidated into a 
+#super class (Hashtags might also work the same way)
+
 class Profile:
     def __init__(self, url):
         self.url = url
@@ -11,7 +19,26 @@ class Profile:
 
     def _scrape_soup(self):
         """Scrape data from the soup"""
-        pass
+        self.title = self.soup.find("title").text
+        
+        prof_json = self._get_json()
+        self._scrape_json(prof_json)
+
+    def _get_json(self):
+        json_script = [str(script) for script in self.soup.find_all('script') if 'config' in str(script)][0]
+        left_index = json_script.find('{')
+        right_index = json_script.rfind('}') + 1
+        json_str = json_script[left_index:right_index]
+        return json.loads(json_str)
+
+    def _scrape_json(self, prof_json):
+        self.country_code = prof_json['country_code']
+        self.language_code = prof_json['language_code']
+        self.locale = prof_json['locale']
+
+        #Convenience definition for post info 
+        self.prof_info = prof_json['entry_data']['ProfilePage'][0]#['graphql']['shortcode_media']
+
 
     @classmethod 
     def from_username(cls, username):
@@ -21,3 +48,4 @@ class Profile:
 if __name__ == '__main__':
     url = r'https://www.instagram.com/chris_greening/'
     profile = Profile(url)
+    profile.load()
