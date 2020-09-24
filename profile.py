cfb1bb4ce73631d9b2c75c5bbec7590633fb5f1d
chris-greening <chrisgreening3@gmail.com>
2020-09-24 15:13:50 -0400

    Subclassed StaticInstaScraper and refactored out respective methods

--- profile.py
+++ profile.py
@@ -1,35 +1,14 @@
-import json
+from __future__ import annotations
 
-import requests 
-from bs4 import BeautifulSoup
+from insta_scraper import StaticInstaScraper
 
-#TODO: A lot of the code from this and Post can probably be consolidated into a 
-#super class (Hashtags might also work the same way)
-
-class Profile:
+class Profile(StaticInstaScraper):
     def __init__(self, url):
-        self.url = url
-    
-    def load(self):
-        """Load the static HTML into a BeautifulSoup object at the url"""
-        self.page_source = requests.get(self.url).text
-        self.soup = BeautifulSoup(self.page_source, features='lxml')
-
-        self._scrape_soup()
+       super().__init__(url)
 
     def _scrape_soup(self):
         """Scrape data from the soup"""
-        self.title = self.soup.find("title").text
-        
-        prof_json = self._get_json()
-        self._scrape_json(prof_json)
-
-    def _get_json(self):
-        json_script = [str(script) for script in self.soup.find_all('script') if 'config' in str(script)][0]
-        left_index = json_script.find('{')
-        right_index = json_script.rfind('}') + 1
-        json_str = json_script[left_index:right_index]
-        return json.loads(json_str)
+        super()._scrape_soup()
 
     def _scrape_json(self, prof_json):
         self.country_code = prof_json['country_code']
@@ -39,7 +18,6 @@ class Profile:
         #Convenience definition for post info 
         self.prof_info = prof_json['entry_data']['ProfilePage'][0]#['graphql']['shortcode_media']
 
-
     @classmethod 
     def from_username(cls, username):
         url = f'https://www.instagram.com/{username}/'
@@ -48,4 +26,4 @@ class Profile:
 if __name__ == '__main__':
     url = r'https://www.instagram.com/chris_greening/'
     profile = Profile(url)
-    profile.load()
+    profile.static_load()
