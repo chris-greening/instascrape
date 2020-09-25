b81646aed0a66e588f5bf004c2975c8dfdaa2f4e
chris-greening <chrisgreening3@gmail.com>
2020-09-25 17:34:56 -0400

    Prepared package for upload to PyPI

--- instascrape/insta_scraper.py
+++ instascrape/insta_scraper.py
@@ -8,6 +8,7 @@ from abc import ABC, abstractmethod
 import requests
 from bs4 import BeautifulSoup
 
+
 class StaticInstaScraper(ABC):
     def __init__(self, url):
         self.url = url
@@ -15,23 +16,27 @@ class StaticInstaScraper(ABC):
     def static_load(self) -> None:
         """Load static HTML at the given URL"""
         self.page_source = requests.get(self.url).text
-        self.soup = BeautifulSoup(self.page_source, features='lxml')
+        self.soup = BeautifulSoup(self.page_source, features="lxml")
 
         self._scrape_soup()
 
     def _scrape_soup(self) -> None:
         """Scrape data from soup, intended to be extendible by subclasses"""
-        self.title = self.soup.find('title').text
+
+        self.title = self.soup.find("title").text
 
         json_data = self._get_json()
         self._scrape_json(json_data)
 
     def _get_json(self) -> dict:
         """Return JSON data as a dictionary"""
-        json_script = [str(script) for script in self.soup.find_all('script')
-                                        if 'config' in str(script)][0]
-        left_index = json_script.find('{')
-        right_index = json_script.rfind('}') + 1
+        json_script = [
+            str(script)
+            for script in self.soup.find_all("script")
+            if "config" in str(script)
+        ][0]
+        left_index = json_script.find("{")
+        right_index = json_script.rfind("}") + 1
         json_str = json_script[left_index:right_index]
         return json.loads(json_str)
 
