98cba761b79836f2a31ac3af9175301280118ea5
Chris <chrisgreening3@gmail.com>
2020-09-25 23:49:11 -0400

    Greatly refactored for future extensibility and features

--- instascrape/insta_scraper.py
+++ instascrape/insta_scraper.py
@@ -8,31 +8,35 @@ from abc import ABC, abstractmethod
 import requests
 from bs4 import BeautifulSoup
 
-
 class StaticInstaScraper(ABC):
     def __init__(self, url):
         self.url = url
 
-    def static_load(self) -> None:
-        """Load static HTML at the given URL"""
-        self.page_source = requests.get(self.url).text
-        self.soup = BeautifulSoup(self.page_source, features="lxml")
+    def static_load(self, session=requests.Session()):
+        self._scrape_url(self.url, session=session)
 
-        self._scrape_soup()
+    def _scrape_url(self, url, session=requests.Session()) -> None:
+        """Load url and scrape data"""
+        page_source = session.get(url).text
+        self._scrape_html(page_source)
 
-    def _scrape_soup(self) -> None:
-        """Scrape data from soup, intended to be extendible by subclasses"""
+    def _scrape_html(self, page_source):
+        soup = BeautifulSoup(page_source, features='lxml')
+        self._scrape_soup(soup)
 
-        self.title = self.soup.find("title").text
-
-        json_data = self._get_json()
+    def _scrape_soup(self, soup):
+        json_data = self._get_json_from_soup(soup)
         self._scrape_json(json_data)
 
-    def _get_json(self) -> dict:
+    @abstractmethod
+    def _scrape_json(self, json_data):
+        pass
+
+    def _get_json_from_soup(self, soup) -> dict:
         """Return JSON data as a dictionary"""
         json_script = [
             str(script)
-            for script in self.soup.find_all("script")
+            for script in soup.find_all("script")
             if "config" in str(script)
         ][0]
         left_index = json_script.find("{")
@@ -40,7 +44,5 @@ class StaticInstaScraper(ABC):
         json_str = json_script[left_index:right_index]
         return json.loads(json_str)
 
-    @abstractmethod
-    def _scrape_json(self, json_data: dict):
-        """Specific implementation of how to scrape JSON left to sublasses"""
-        pass
+
+
