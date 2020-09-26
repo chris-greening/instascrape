25c51a80a323cd29e54d9ef327a272772763a825
chris-greening <chrisgreening3@gmail.com>
2020-09-26 15:39:52 -0400

    Added proper documentation to class and public methods

--- instascrape/profile.py
+++ instascrape/profile.py
@@ -5,7 +5,28 @@ from .jsontools import ProfileJSON
 
 
 class Profile(StaticInstaScraper):
+    """
+    Representation of an Instagram profile page.
+
+    Attribues
+    ---------
+    url : str
+        Full URL to an existing Instagram profile
+
+    Methods
+    -------
+    static_load(session=requests.Session())
+        Makes request to URL, instantiates BeautifulSoup, finds JSON data,
+        then parses JSON data.
+    """
+
     def __init__(self, url):
+        """
+        Parameters
+        ----------
+        url : str
+            Full URL to an existing Instagram profile
+        """
         super().__init__(url)
 
     def _scrape_json(self, json_data: dict):
@@ -14,6 +35,26 @@ class Profile(StaticInstaScraper):
 
     @classmethod
     def from_username(cls, username: str):
+        """
+        Factory method for convenience to create Profile instance given
+        just a username instead of a full URL.
+
+        Parameters
+        ----------
+        username : str
+            Username of the Profile for scraping
+
+        Returns
+        -------
+        Profile(url)
+            Instance of Profile with URL at the given username
+
+        Example
+        -------
+        >>>Profile.from_username('gvanrossum')
+        <https://www.instagram.com/gvanrossum/: Profile>
+        """
+
         url = f"https://www.instagram.com/{username}/"
         return cls(url)
 
