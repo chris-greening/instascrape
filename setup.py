cf3ecca71d8a43b5653954a0de20975da58932c1
chris-greening <chrisgreening3@gmail.com>
2020-09-26 15:05:59 -0400

    Changed version 0.0.6->0.0.7

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="insta-scrape",  
-    version="0.0.6",
+    name="insta-scrape",
+    version="0.0.7",
     author="Chris Greening",
     author_email="chris@christophergreening.com",
     description="Super lightweight Instagram web scraper for data analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/chris-greening/instascrape",
-    packages=['instascrape'],
-    install_requires=['requests', 'beautifulsoup4'],
+    packages=["instascrape"],
+    install_requires=["requests", "beautifulsoup4"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
