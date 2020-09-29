66420a1478a327ab89c0a3824821b21f158547ef
chris-greening <chrisgreening3@gmail.com>
2020-09-29 01:45:34 -0400

    Created dynamic web scraping example

--- examples/profile_scrape/dynamic_profile.py
+++ examples/profile_scrape/dynamic_profile.py
@@ -3,10 +3,11 @@ import sys
 import time 
 
 from pandas import DataFrame
+import numpy as np 
 from selenium.webdriver import Chrome, ChromeOptions
 from bs4 import BeautifulSoup
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(r'..\..'))
 from instascrape import Profile, Post
 
 chrome_options = ChromeOptions()
@@ -14,12 +15,12 @@ prefs = {"profile.managed_default_content_settings.images": 2}
 chrome_options.add_experimental_option("prefs", prefs)
 
 class DynamicProfile(Profile):
-    """Subclass of Profile to provide some dynamic functionality"""
-    def get_all_posts(self):
+    """Subclass of Profile to provide some dynamic functionality using Selenium"""
+    def dynamic_load(self):
         """Scroll the users Profile page and load all posts into Post objects"""
         source_data, browser = self._scroll_page()
         post_soup = self._seperate_posts(source_data)
-        self._create_post_object(post_soup)
+        self._create_post_objects(post_soup)
         self._grab_useful_data()
         browser.close()
         
@@ -34,7 +35,7 @@ class DynamicProfile(Profile):
         lenOfPage = browser.execute_script(js_script)
         match=False
         while not match:
-            time.sleep(3)
+            time.sleep(np.random.normal(3, 1.5))
             lastCount = lenOfPage
             lenOfPage = browser.execute_script(js_script)
             source_data.append(browser.page_source)
@@ -47,7 +48,7 @@ class DynamicProfile(Profile):
         post_soup = []
         for source in source_data:
             soup = BeautifulSoup(source, features='lxml')
-            posts = soup.find("span", {"id":"react-root"})
+            # posts = soup.find("span", {"id":"react-root"})
 
             anchor_tags = soup.find_all("a")
             found = [tag for tag in anchor_tags if tag.find("div", {"class":"eLAPa"})]
@@ -57,19 +58,23 @@ class DynamicProfile(Profile):
                     post_soup.append(tag)
         return post_soup
 
-    def _create_post_object(self, post_soup):
+    def _create_post_objects(self, post_soup):
         #create post objects for each post on the page 
         self.posts = []
         for post in post_soup:
-            shortcode = post["href"]
-            shortcode = shortcode.replace("/p/", "")
-            shortcode = shortcode[:-1]
+            shortcode = post["href"].replace("/p/", "")[:-1]
             self.posts.append(Post.from_shortcode(shortcode))
 
     def _grab_useful_data(self):
-        for post in self.posts: 
-            post.static_load()
+        for i, post in enumerate(self.posts): 
+            if i % 10 == 0:
+                print(i)  
+            time.sleep(3)
+            try:
+                post.static_load()
+            except Exception as e:
+                print(e)
 
 if __name__ == '__main__':
     chris = DynamicProfile.from_username('chris_greening')
-    chris.get_all_posts()
+    chris.dynamic_load()
