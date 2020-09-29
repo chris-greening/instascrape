fc0213645bfe52f42f57486f947c3cb5a9a20926
chris-greening <chrisgreening3@gmail.com>
2020-09-29 15:29:55 -0400

    Added type hints and some documentation to JSONData

--- instascrape/jsontools.py
+++ instascrape/jsontools.py
@@ -2,7 +2,7 @@ import json
 from abc import ABC, abstractmethod
 import datetime
 
-from typing import List
+from typing import List, Any
 
 #TODO: create args in parse_json that define what attributes exist for each
 # class after scraped, might be worth defining as dataclasses or creating separate
@@ -11,49 +11,96 @@ from typing import List
 # all in one fell swoop
 
 class JSONScraper(ABC):
+    """
+    Abstract base class containing methods for handling and parsing Instagram 
+    JSON data served back in static GET requests
+
+    Attributes 
+    ----------
+    METADATA_KEYS : list 
+        List of strings containing attributes that are considered metadata 
+        regarding the JSON data but are not actual data points scraped from the 
+        JSON itself
+    json_dict : dict
+        Python dictionary containing the Instagram JSON data
+    name : str, optional 
+        Custom name that will represent this JSON data
+
+    Methods 
+    -------
+    parse_json() -> None
+        Abstract method, parses the JSON data specific to the Instagram object 
+        type
+    load_value(data_dict: dict, key: str, fail_return: Any=None)
+        Returns value in dictionary at the specified key. If value doesn't 
+        exist, returns a default value
+    from_json_string(json_str: str, name: str = None)
+        Loads a json string as a dictionary and returns a JSONData object with 
+        that dictionary.
+    from_json_file(json_fpath: str, name: str = None)
+        Loads a json file at a given JSON filepath into a dictionary and 
+        returns a JSONData object with that dictionary
+    """ 
 
     METADATA_KEYS = ['json_dict', 'name']
 
-    def __init__(self, json_dict: dict, name: str = None):
+    def __init__(self, json_dict: dict, name: str = None) -> None:
         """Container for storing all scraped data from Instagram JSON"""
         self.json_dict = json_dict
         if name is not None:
             self.name = name
 
     @abstractmethod
-    def parse_json(self):
+    def parse_json(self) -> None:
         """Parse JSON object"""
 
     @property
-    def scraped_attr(self):
+    def scraped_attr(self) -> List[str]:
         """Return list of names of attributes that have been scraped from the JSON"""
         return [attr for attr in self.__dict__ if attr not in JSONScraper.METADATA_KEYS]
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         class_name = type(self).__name__
         output_str = "<{}: " + f"{class_name}>"
         if hasattr(self, "name"):
             return output_str.format(self.name)
         return output_str.format("unnamed")
 
-    def load_value(self, data_dict, key):
+    def load_value(self, data_dict: dict, key: str, fail_default: Any = None) -> Any:
+        """
+        Returns the value of a dictionary at a given key, returning a specified 
+        value if the key does not exsit.
+
+        Parameters
+        ----------
+        data_dict : dict 
+            Dictionary of key: val pairs that you want to look for 
+        key : str 
+            Key in dictionary to search for 
+        fail_return : Any, optional 
+
+        Returns 
+        -------
+        return_val : Any 
+            Value or default return of the dictionary lookup
+        """
         try: 
-            return data_dict[key]
+            return_val = data_dict[key]
         except KeyError: 
-            return None
+            return_val = fail_default
+        return return_val
 
     @classmethod
-    def from_json_string(cls, json_string: str, name: str):
+    def from_json_string(cls, json_string: str, name: str = None):
         """Load JSON data object from a JSON string"""
         return cls(json.loads(json_string), name)
 
     @classmethod
-    def from_json_file(cls, json_fpath: str, name: str):
+    def from_json_file(cls, json_fpath: str, name: str = None):
         """Load JSON data object from a JSON file"""
         with open(json_fpath, 'r') as infile:
             json_data = json.load(infile)
         return cls(json_data, name)
-
 
 class ProfileJSON(JSONScraper):
     def parse_json(self):
@@ -112,7 +159,6 @@ class HashtagJSON(JSONScraper):
         self.is_top_media_only = tag_data["is_top_media_only"]
         self.profile_pic_url = tag_data["profile_pic_url"]
         self.amount_of_posts = tag_data["edge_hashtag_to_media"]["count"]
-
 
 class PostJSON(JSONScraper):
     def parse_json(self):
