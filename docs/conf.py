81e5433f16fa73089fcd48107632bfe75b1bb141
chris-greening <chrisgreening3@gmail.com>
2020-09-29 16:46:34 -0400

    Changed sys.path.insert

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../instascrape'))
+sys.path.insert(0, os.path.abspath('..'))
 # sys.path.insert(0, r'D:\Programming\pythonstuff\instascrape')
 
 
