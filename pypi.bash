b509d5f64e38b4503ea02372041d807a65a581ec
Christopher Greening <36808060+chris-greening@users.noreply.github.com>
2020-10-06 01:38:25 -0400

    Added comments

--- pypi.bash
+++ pypi.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#Delete old data
+#Automate the process of uploading package to PyPI
 
+#Delete older version build directories
 directories=("dist/" "build/" "insta_scrape.egg-info/")
-
 for d in ${directories[@]};
 do
 	if [ -d $d ];
@@ -13,6 +13,6 @@ do
 	fi
 done
 
+#Setup package and initiate upload to PyPI
 python3 setup.py sdist bdist_wheel
 twine upload dist/*
-
