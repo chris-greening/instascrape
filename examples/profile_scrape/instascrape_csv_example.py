f3bccfa72744a9c14220dd7cb2135eb828cbd6dd
Beckett Normington <beckett@chatter-social.com>
2020-10-08 19:58:50 -0400

    add comments to example
    black styling needed

--- examples/profile_scrape/instascrape_csv_example.py
+++ examples/profile_scrape/instascrape_csv_example.py
@@ -1,22 +1,22 @@
-import json
+import json # import needed libraries / pip3 install json
 
-from instascrape import Profile
+from instascrape import Profile # pip3 install insta-scrape
 
-google = Profile.from_username('google')
+import pandas as pd # pip3 install pandas
 
-google.static_load()
+google = Profile.from_username('google') # declare profile
 
-google_data = google.to_dict()
+google.static_load() # scrape profile
 
-import pandas as pd
+google_data = google.to_dict() # turn scraped data into python dictionary
 
-google_serialized = json.dumps(google_data)
+google_serialized = json.dumps(google_data) # serialize dictionary into json
 
-with open('google.json', 'w') as outfile:
-     json.dump(google_data, outfile)
+with open('google.json', 'w') as outfile: # write json data to "google.json"
+     json.dump(google_data, outfile) 
 
-with open('google.json', encoding='utf-8-sig') as f_input:
+with open('google.json', encoding='utf-8-sig') as f_input: # read json data from "google.json" and create pandas dataframe
     df = pd.read_json(f_input)
 
-df.to_csv('google.csv', encoding='utf-8', index=False)
+df.to_csv('google.csv', encoding='utf-8', index=False) # convert dataframe to csv and write to "google.csv"
 
