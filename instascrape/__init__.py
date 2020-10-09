9b4863c0f1b48f49117cc011aa5ebba22e50a445
chris-greening <chrisgreening3@gmail.com>
2020-10-09 01:00:00 -0400

    Added convenience imports

---
+++ instascrape/__init__.py
@@ -0,0 +1,7 @@
+from .hashtag import *
+from .http_error_page import *
+from .landing_page import *
+from .landing_page import *
+from .login_and_signup import *
+from .post import *
+from .profile import *
