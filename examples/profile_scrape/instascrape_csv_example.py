8a1f7ffe7d6b9954d95cf03a0b75c065e16b2917
Christopher Greening <36808060+chris-greening@users.noreply.github.com>
2020-10-08 21:27:37 -0400

    Update examples/profile_scrape/instascrape_csv_example.py

--- examples/profile_scrape/instascrape_csv_example.py
+++ examples/profile_scrape/instascrape_csv_example.py
@@ -10,13 +10,7 @@ google.static_load() # scrape profile
 
 google_data = google.to_dict() # turn scraped data into python dictionary
 
-google_serialized = json.dumps(google_data) # serialize dictionary into json
-
-with open('google.json', 'w') as outfile: # write json data to "google.json"
-     json.dump(google_data, outfile) 
-
-with open('google.json', encoding='utf-8-sig') as f_input: # read json data from "google.json" and create pandas dataframe
-    df = pd.read_json(f_input)
+google_data = {key: [val] for key, val in google_data.items()}   
+df = pd.DataFrame(google_data)
 
 df.to_csv('google.csv', encoding='utf-8', index=False) # convert dataframe to csv and write to "google.csv"
-
