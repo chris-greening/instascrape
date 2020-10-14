a3b6b1418faca7285e9d65257e48949dbabacfad
chris-greening <chrisgreening3@gmail.com>
2020-10-14 02:39:37 -0400

    Fixed get_recent_posts

--- instascrape/scrapers/profile.py
+++ instascrape/scrapers/profile.py
@@ -31,7 +31,7 @@ class Profile(static_scraper.StaticHTMLScraper):
     def get_recent_posts(self):
         """Get data from the 12 most recent posts"""
         self.posts = []
-        for post_json in self.data.json_dict['entry_data']['ProfilePage'][0][
+        for post_json in self.recent_data.json_dict['entry_data']['ProfilePage'][0][
             'graphql']['user']['edge_owner_to_timeline_media']['edges']:
             post = PostJSON()
             post.parse_from_profile(post_json['node'], exception=False)
