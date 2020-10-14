e2b01233072790930995c47058d50df5b6d37f8c
chris-greening <chrisgreening3@gmail.com>
2020-10-14 01:33:55 -0400

    Rewrote to include new syntax

--- tests/test_post.py
+++ tests/test_post.py
@@ -4,15 +4,14 @@ import pytest
 
 from instascrape import Post
 
-
 @pytest.fixture
 def google_post() -> Post:
-    google_post_url = f"https://www.instagram.com/p/B_VEzbplCFT/"
+    google_post_url = f"https://www.instagram.com/p/CAvM5gIp9xX/"
     random_google_post = Post(url=google_post_url)
     random_google_post.static_load()
     return random_google_post
 
-
+@pytest.mark.scrape_hashtags
 def test_scrape_hashtags(google_post, capsys):
     expected_hashtags = ["stayhome", "mysuperg"]
 
@@ -20,10 +19,9 @@ def test_scrape_hashtags(google_post, capsys):
 
     assert all(hashtags in expected_hashtags for hashtags in google_post.hashtags)
 
-
 def test_from_shortcode(google_post):
-    result_post: Post = Post.from_shortcode(shortcode=google_post.shortcode)
+    result_post: Post = Post.from_shortcode(shortcode=google_post.data_points[0].shortcode)
     result_post.static_load()
 
     assert result_post.url == google_post.url
-    assert result_post.caption == google_post.caption
+    assert result_post.data_points[0].caption == google_post.data_points[0].caption
