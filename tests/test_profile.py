d59e66d73da309e9ea2274e694b86f53c38497dd
chris-greening <chrisgreening3@gmail.com>
2020-10-14 02:40:53 -0400

    Added test_get_recent_posts

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,11 +1,22 @@
+import pytest
+
 from instascrape import Profile
 
+@pytest.fixture(scope='module')
+def profile() -> Profile:
+    profile_url = "https://www.instagram.com/chris_greening/"
+    profile_obj = Profile(profile_url)
+    profile_obj.static_load()
+    return profile_obj
 
-def test_from_username():
-    expected_profile_usename = "instagram"
-    expected_profile_url = f"https://www.instagram.com/{expected_profile_usename}/"
+def test_get_recent_posts(profile):
+    profile.get_recent_posts()
+    assert len(profile.posts) == 12
 
-    result_profile: Profile = Profile.from_username(username=expected_profile_usename)
+def test_from_username(profile):
 
-    assert result_profile.name == expected_profile_usename
-    assert result_profile.url == expected_profile_url
+    expected_profile_username = 'chris_greening'
+    result_profile: Profile = Profile.from_username(username=expected_profile_username)
+
+    assert result_profile.name == profile.data_points[0].username
+    assert result_profile.url == profile.url
