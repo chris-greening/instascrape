c4a267361950474be8b24a820ae2c39f374c681a
Michael Lam <o3661606@gmail.com>
2020-10-16 18:09:37 +0800

    Add selenium as dependency

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/chris-greening/instascrape",
     packages=["instascrape"],
-    install_requires=["requests", "beautifulsoup4"],
+    install_requires=["requests", "beautifulsoup4", "selenium"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
