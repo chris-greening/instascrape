521fc527d6ac401cf0aa2516d8bde02c5d766478
chris-greening <chrisgreening3@gmail.com>
2020-10-16 03:22:52 -0400

    Added class docstrings and type hints

--- instascrape/scrapers/hashtag.py
+++ instascrape/scrapers/hashtag.py
@@ -8,28 +8,23 @@ from instascrape.core._static_scraper import _StaticHtmlScraper
 from instascrape.core._mappings import _HashtagMapping
 
 class Hashtag(_StaticHtmlScraper):
+    """
+    Scraper for an Instagram hashtag page
+
+    Attributes
+    ----------
+    _Mapping
+        Mapping class with directives specific to scraping JSON from an
+        Instagram hashtag page
+
+    Methods
+    -------
+    from_hashtag(shortcode: str) -> Post
+        Factory method that returns a Hashtag object from a shortcode
+    """
     _Mapping = _HashtagMapping
 
     @classmethod
-    def from_hashtag(cls, hashtag: str):
-        """
-        Factory method for convenience to create Hashtag instance given
-        just a hashtag name instead of a full URL.
-
-        Parameters
-        ----------
-        hashtag : str
-            Name of the Hashtag for scraping
-
-        Returns
-        -------
-        Hashtag(url: str)
-            Instance of Hashtag with URL created from the given hashtag name
-
-        Example
-        -------
-        >>>Hashtag.from_hashtag('pythonprogramming')
-        <https://www.instagram.com/tags/pythonprogramming/: Hashtag>
-        """
+    def from_hashtag(cls, hashtag: str) -> Hashtag:
         url = f"https://www.instagram.com/tags/{hashtag}/"
         return cls(url, name=hashtag)
--- instascrape/scrapers/profile.py
+++ instascrape/scrapers/profile.py
@@ -6,10 +6,24 @@ from instascrape.core._static_scraper import _StaticHtmlScraper
 from instascrape.core._mappings import _ProfileMapping
 
 class Profile(_StaticHtmlScraper):
+    """
+    Scraper for an Instagram profile page
+
+    Attributes
+    ----------
+    _Mapping
+        Mapping class with directives specific to scraping JSON from an
+        Instagram profile page
+
+    Methods
+    -------
+    from_username(shortcode: str) -> Post
+        Factory method that returns a Profile object from a shortcode
+    """
     _Mapping = _ProfileMapping
 
     @classmethod
-    def from_username(cls, username: str):
+    def from_username(cls, username: str) -> Profile:
         """
         Factory method for convenience to create Profile instance given
         just a username instead of a full URL.
