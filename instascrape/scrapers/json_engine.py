0dfa6b6287d160716c3871b71b6c680cc34d2221
chris-greening <chrisgreening3@gmail.com>
2020-10-15 22:13:29 -0400

    Finished ProfileMapping

--- instascrape/scrapers/json_engine.py
+++ instascrape/scrapers/json_engine.py
@@ -29,10 +29,49 @@ class GeneralMapping:
             keys = list(cls.mapping)
         return {key: cls.mapping[key] for key in keys}
 
-
-class ProfileMapping:
-    pass
-    # mapping.update(GeneralMapping.mapping)
+class ProfileMapping(GeneralMapping):
+    mapping = GeneralMapping.return_mapping().copy()
+    profile_page = ['entry_data', 'ProfilePage', 0]
+    user = profile_page + ['graphql', 'user']
+    mapping.update({
+        'logging_page_id': deque(profile_page + ['logging_page_id']),
+        'show_suggested_profiles': deque(profile_page + ['show_suggested_profiles']),
+        'show_follow_dialog': deque(profile_page + ['show_follow_dialog']),
+        'biography': deque(user + ['biography']),
+        'blocked_by_viewer': deque(user + ['blocked_by_viewer']),
+        'business_email': deque(user + ['business_email']),
+        'restricted_by_viewer': deque(user + ['restricted_by_viewer']),
+        'country_block': deque(user + ['country_block']),
+        'external_url': deque(user + ['external_url']),
+        'external_url_linkshimmed': deque(user + ['external_url_linkshimmed']),
+        'followers': deque(user + ['edge_followed_by', 'count']),
+        'followed_by_viewer': deque(user + ['followed_by_viewer']),
+        'following': deque(user + ['edge_follow', 'count']),
+        'follows_viewer': deque(user + ['follows_viewer']),
+        'full_name': deque(user + ['full_name']),
+        'has_ar_effects': deque(user + ['has_ar_effects']),
+        'has_clips': deque(user + ['has_clips']),
+        'has_guides': deque(user + ['has_guides']),
+        'has_channel': deque(user + ['has_channel']),
+        'has_blocked_viewer': deque(user + ['has_blocked_viewer']),
+        'highlight_reel_count': deque(user + ['highlight_reel_count']),
+        'has_requested_viewer': deque(user + ['has_requested_viewer']),
+        'id': deque(user + ['id']),
+        'is_business_account': deque(user + ['is_business_account']),
+        'is_joined_recently': deque(user + ['is_joined_recently']),
+        'business_category_name': deque(user + ['business_category_name']),
+        'overall_category_name': deque(user + ['overall_category_name']),
+        'category_enum': deque(user + ['category_enum']),
+        'is_private': deque(user + ['is_private']),
+        'is_verified': deque(user + ['is_verified']),
+        'mutual_followers': deque(user + ['edge_mutual_followed_by', 'count']),
+        'profile_pic_url': deque(user + ['profile_pic_url']),
+        'profile_pic_url_hd': deque(user + ['profile_pic_url_hd']),
+        'requested_by_viewer': deque(user + ['requested_by_viewer']),
+        'username': deque(user + ['username']),
+        'connected_fb_page': deque(user + ['connected_fb_page']),
+        'posts': deque(user + ['edge_owner_to_timeline_media', 'count']),
+    })
 
 class JsonEngine:
     """
@@ -49,23 +88,21 @@ class JsonEngine:
         self._fill_default_values()
 
     def _fill_default_values(self):
+        """Loop through each key in map_dict and set value given a list of directives"""
         for key in self.map_dict:
             self._set_value(key, self.json_data, self.map_dict[key])
 
     def _set_value(self, orig_key: str, container: dict, directive_queue: deque):
+        """Recursively set each value from the mapping"""
         current_key = directive_queue.popleft()
         value = container[current_key]
-        print(value)
         if len(directive_queue) == 0:
             setattr(self, orig_key, value)
         else:
             self._set_value(orig_key, value, directive_queue)
 
-    def scrape_data_point(self, key, directive: List[Union[str, int]]):
-        pass
-
 if __name__ == '__main__':
     from instascrape import json_from_url
 
     json_dict = json_from_url('https://www.instagram.com/chris_greening')
-    data = JsonEngine(json_dict, GeneralMapping.return_mapping())
+    data = JsonEngine(json_dict, ProfileMapping.return_mapping())
