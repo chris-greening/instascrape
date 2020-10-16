cab0919a1560ac13d89442cbaf761255dfa0671c
Chris <chrisgreening3@gmail.com>
2020-10-16 00:22:08 -0400

    Updated tests to reflect new changes

--- tests/test_post.py
+++ tests/test_post.py
@@ -8,25 +8,11 @@ from instascrape import Post
 def google_post() -> Post:
     google_post_url = f"https://www.instagram.com/p/CFkIz2UlIng/"
     random_google_post = Post(url=google_post_url)
-    random_google_post.static_load()
+    random_google_post.load()
     return random_google_post
 
-@pytest.mark.scrape_hashtags
-def test_scrape_hashtags(google_post, capsys):
-    expected_hashtags = ["smallbusinessweek", "growwithgoogle"]
-
-    hashtag_arr = google_post.scrape_hashtags(status_output=True)
-    assert all([hashtag.data_points[0].name in expected_hashtags for hashtag in hashtag_arr])
-
 def test_from_shortcode(google_post):
-    result_post: Post = Post.from_shortcode(shortcode=google_post.data_points[0].shortcode)
-    result_post.static_load()
+    result_post: Post = Post.from_shortcode(shortcode=google_post.shortcode)
 
     assert result_post.url == google_post.url
-    assert result_post.data_points[0].caption == google_post.data_points[0].caption
 
-def test_tagged_users(google_post):
-    expected_tags = ["jimmyfallon", "fallontonight"]
-
-    tagged_arr = google_post.data_points[0].get_tagged_users()
-    assert all([tag in expected_tags for tag in tagged_arr])
