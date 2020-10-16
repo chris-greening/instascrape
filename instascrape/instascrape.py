f78d5eae57e03f17a53ef862f405cf0722f2aab9
chris-greening <chrisgreening3@gmail.com>
2020-10-16 00:46:17 -0400

    Changed method name

--- instascrape/instascrape.py
+++ instascrape/instascrape.py
@@ -8,15 +8,15 @@ from instascrape.scrapers import *
 class InstaScrape:
     def scrape_profile(self, username: str) -> Dict[str, str]:
         profile_obj = Profile.from_username(username)
-        profile_obj.static_load()
+        profile_obj.load()
         return profile_obj.recent_data.to_dict()
 
     def scrape_post(self, url: str) -> Dict[str, str]:
         post_obj = Post(url)
-        post_obj.static_load()
+        post_obj.load()
         return post_obj.recent_data.to_dict()
 
     def scrape_hashtag(self, hashtag: str) -> Dict[str, str]:
         hashtag_obj = Hashtag.from_hashtag(hashtag)
-        hashtag_obj.static_load()
+        hashtag_obj.load()
         return hashtag_obj.recent_data.to_dict()
