6d2684c5ec163228ab716b64ed1dd2344380d06a
chris-greening <chrisgreening3@gmail.com>
2020-10-15 23:58:28 -0400

    Reimplemented StaticHTMLScraper to use the JsonScraper internally

--- instascrape/scrapers/static_scraper.py
+++ instascrape/scrapers/static_scraper.py
@@ -8,25 +8,10 @@ from abc import ABC, abstractmethod
 import requests
 from bs4 import BeautifulSoup
 
+from instascrape.scrapers.json_scraper import JsonScraper
+from instascrape.scrapers.mappings import MetaMapping
+
 class StaticHTMLScraper(ABC):
-    """
-    Abstract base class for the Profile, Post, and Hashtag subclasses
-    that handles much of the routine scraping methods
-
-    Attributes
-    ---------
-    url : str
-        Full URL to an Instagram page
-
-    Methods
-    -------
-    static_load(session=requests.Session())
-        Makes request to URL, instantiates BeautifulSoup, finds JSON data,
-        then parses JSON data.
-    """
-
-    _METADATA_KEYS = ["url", "name", "data", 'page_source', 'soup', 'scrape_timestamp']
-
     def __init__(self, url, name=None):
         """
         Parameters
@@ -35,74 +20,17 @@ class StaticHTMLScraper(ABC):
             Full URL to an Instagram page
         """
         self.url = url
-        if name is not None:
-            self.name = name
-        self.data_points = []
+        self._json_scraper = JsonScraper()
 
-    def static_load(self, session=requests.Session()):
-        self._scrape_url(self.url, session=session)
+    def load(self):
+        json_dict = self._json_scraper.json_from_url(self.url)
+        mapper = MetaMapping.get_mapper(json_dict)
+        scraped_dict = self._json_scraper.parse_json(json_dict=json_dict, map_dict=mapper.return_mapping())
+        self._load_into_namespace(scraped_dict=scraped_dict)
 
-    def to_dict(self) -> dict:
-        """Return a dictionary containing all of the data that has been scraped"""
-        return {
-            key: val
-            for key, val in self.__dict__.items()
-            if key not in StaticHTMLScraper._METADATA_KEYS
-        }
+    def _load_into_namespace(self, scraped_dict):
+        for key, val in scraped_dict.items():
+            setattr(self, key, val)
 
-    def to_list(self) -> List:
-        return [
-            (key, val)
-            for key, val in self.__dict__.items()
-            if key not in StaticHTMLScraper._METADATA_KEYS
-        ]
 
-    def _scrape_url(self, url, session=requests.Session()) -> None:
-        """Load url and scrape data"""
-        self.page_source = session.get(url).text
-        self._scrape_html(self.page_source)
 
-    def _scrape_html(self, page_source):
-        self.soup = BeautifulSoup(page_source, features="lxml")
-        self._scrape_soup(self.soup)
-
-    def _scrape_soup(self, soup):
-        json_data = self._get_json_from_soup(soup)
-        self._scrape_json(json_data)
-
-    def _get_json_from_soup(self, soup) -> dict:
-        """Return JSON data as a dictionary"""
-        json_script = [
-            str(script) for script in soup.find_all("script") if "config" in str(script)
-        ][0]
-        left_index = json_script.find("{")
-        right_index = json_script.rfind("}") + 1
-        json_str = json_script[left_index:right_index]
-        return json.loads(json_str)
-
-    def _load_json_into_namespace(self, data):
-        """Parse and load JSON data into objects namespace"""
-        self.__dict__.update(data.to_dict())
-
-    def _scrape_json(self, json_dict):
-        """
-        Specific way of scraping the JSON data is left up to the subclasses
-        """
-        self.recent_data = self.AssociatedJSON()
-        self.recent_data.parse_full(json_dict)
-        self.data_points.append(self.recent_data)
-
-    def clear(self):
-        """Clear all collected data points"""
-        self.data_points = []
-
-    @property
-    def AssociatedJSON(self):
-        raise NotImplementedError
-
-    @classmethod
-    def set_associated_json(cls, AssociatedJSON):
-        cls.AssociatedJSON = AssociatedJSON
-
-    def __repr__(self):
-        return f"<{self.url}: {type(self).__name__}>"
