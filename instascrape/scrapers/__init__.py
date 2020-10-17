31577f3d0dc4251c4ae27369ff5f79f1a2601a6d
chris-greening <chrisgreening3@gmail.com>
2020-10-17 00:41:39 -0400

    Added JsonScraper to convenience import to make avail to user

--- instascrape/scrapers/__init__.py
+++ instascrape/scrapers/__init__.py
@@ -2,3 +2,5 @@ from instascrape.scrapers.profile import *
 from instascrape.scrapers.post import *
 from instascrape.scrapers.hashtag import *
 from instascrape.scrapers.login import *
+
+from instascrape.scrapers.json_scraper import *
