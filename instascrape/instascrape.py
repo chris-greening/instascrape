befc9115209503069015997c77146cd553342504
chris-greening <chrisgreening3@gmail.com>
2020-10-17 01:37:19 -0400

    Fixed return type

--- instascrape/instascrape.py
+++ instascrape/instascrape.py
@@ -8,17 +8,17 @@ from instascrape.scrapers import *
 
 
 class InstaScrape:
-    def load_profile(self, username: str) -> Dict[str, str]:
+    def load_profile(self, username: str) -> Profile:
         profile = Profile.from_username(username)
         profile.load()
         return profile
 
-    def load_post(self, url: str) -> Dict[str, str]:
+    def load_post(self, url: str) -> Post:
         post = Post(url)
         post.load()
         return post
 
-    def load_hashtag(self, hashtag: str) -> Dict[str, str]:
+    def load_hashtag(self, hashtag: str) -> Hashtag:
         hashtag = Hashtag.from_hashtag(hashtag)
         hashtag.load()
         return hashtag
