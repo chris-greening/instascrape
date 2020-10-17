b95cd861d3a09c5d08272443fc54417c0a702163
chris-greening <chrisgreening3@gmail.com>
2020-10-17 02:20:18 -0400

    Added docstrings

--- instascrape/scrapers/hashtag.py
+++ instascrape/scrapers/hashtag.py
@@ -12,11 +12,6 @@ from instascrape.core._mappings import _HashtagMapping
 class Hashtag(_StaticHtmlScraper):
     """
     Scraper for an Instagram hashtag page
-
-    Methods
-    -------
-    from_hashtag(shortcode: str) -> Post
-        Factory method that returns a Hashtag object from a shortcode
     """
 
     _Mapping = _HashtagMapping
@@ -25,3 +20,8 @@ class Hashtag(_StaticHtmlScraper):
     def from_hashtag(cls, hashtag: str) -> Hashtag:
         url = f"https://www.instagram.com/tags/{hashtag}/"
         return cls(url, name=hashtag)
+
+#Set docstrings for Hashtag so that they appear in sphinx documentation
+Hashtag.to_json.__doc__ == _StaticHtmlScraper.to_json.__doc__
+Hashtag.to_csv.__doc__ == _StaticHtmlScraper.to_csv.__doc__
+Hashtag.to_dict.__doc__ == _StaticHtmlScraper.to_dict.__doc__
