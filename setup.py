1d4fbb44202bd0c0f23b6ec79819a28d34d5ce32
chris-greening <chrisgreening3@gmail.com>
2020-10-18 22:27:14 -0400

    bug: Fixed PyPI install bug, submodules weren't included

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="insta-scrape",
-    version="0.6.0",
+    version="0.6.7",
     author="Chris Greening",
     author_email="chris@christophergreening.com",
     description="Super lightweight Instagram web scraper for data analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/chris-greening/instascrape",
-    packages=["instascrape"],
+    packages=["instascrape", "instascrape.core", "instascrape.scrapers"],
     install_requires=["requests", "beautifulsoup4"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )
