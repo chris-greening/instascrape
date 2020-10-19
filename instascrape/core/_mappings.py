409d058baeaf977c080ff3d6bf4b465474c95f6e
chris-greening <chrisgreening3@gmail.com>
2020-10-19 17:16:05 -0400

    Rewrote directives to reflect new usage of JSON flattening algo prior to needing keys

--- instascrape/core/_mappings.py
+++ instascrape/core/_mappings.py
@@ -14,8 +14,9 @@ MappingObject = Union[
 
 class _GeneralMapping(ABC):
     """
-    Maps the user interfacing attribute names with a directive for parsing that
-    data point from JSON data
+    Maps the user interfacing attribute names with their keys as given in a JSON
+    dict that has been flattened using
+    instascrape.core._json_flattener.JsonFlattener
 
     Attributes
     ----------
@@ -29,19 +30,20 @@ class _GeneralMapping(ABC):
         Interface for returning only mapping directives that are specified in
         a list of keys
 
-    """ """General mappings to the JSON data that are present in all JSON data"""
+    """
 
     mapping = {
-        "config": deque(["config", "csrf_token"]),
-        "viewer": deque(["config", "viewer"]),
+        "csrf_token": deque(['csrf_token']),
+        "viewer": deque(["viewer"]),
+        "viewer_id": deque(['viewerId']),
         "country_code": deque(["country_code"]),
         "language_code": deque(["language_code"]),
         "locale": deque(["locale"]),
         "device_id": deque(["device_id"]),
         "browser_push_pub_key": deque(["browser_push_pub_key"]),
-        "key_id": deque(["encryption", "key_id"]),
-        "public_key": deque(["encryption", "public_key"]),
-        "version": deque(["encryption", "version"]),
+        "key_id": deque(["key_id"]),
+        "public_key": deque(["public_key"]),
+        "version": deque(["version"]),
         "is_dev": deque(["is_dev"]),
         "rollout_hash": deque(["rollout_hash"]),
         "bundle_variant": deque(["bundle_variant"]),
@@ -72,7 +74,6 @@ class _GeneralMapping(ABC):
             keys = [key for key in keys if key not in exclude]
         directive_dict = {key: deepcopy(cls.mapping[key]) for key in keys}
         return directive_dict
-
 
 class _PostMapping(_GeneralMapping):
     """Mapping specific to Instagram post pages"""
@@ -143,52 +144,47 @@ class _ProfileMapping(_GeneralMapping):
     """Mapping specific to Instagram profile pages"""
 
     mapping = _GeneralMapping.return_mapping().copy()
-    profile_page = ["entry_data", "ProfilePage", 0]
-    user = profile_page + ["graphql", "user"]
     mapping.update(
         {
-            "logging_page_id": deque(profile_page + ["logging_page_id"]),
-            "show_suggested_profiles": deque(
-                profile_page + ["show_suggested_profiles"]
-            ),
-            "show_follow_dialog": deque(profile_page + ["show_follow_dialog"]),
-            "biography": deque(user + ["biography"]),
-            "blocked_by_viewer": deque(user + ["blocked_by_viewer"]),
-            "business_email": deque(user + ["business_email"]),
-            "restricted_by_viewer": deque(user + ["restricted_by_viewer"]),
-            "country_block": deque(user + ["country_block"]),
-            "external_url": deque(user + ["external_url"]),
-            "external_url_linkshimmed": deque(user + ["external_url_linkshimmed"]),
-            "followers": deque(user + ["edge_followed_by", "count"]),
-            "followed_by_viewer": deque(user + ["followed_by_viewer"]),
-            "following": deque(user + ["edge_follow", "count"]),
-            "follows_viewer": deque(user + ["follows_viewer"]),
-            "full_name": deque(user + ["full_name"]),
-            "has_ar_effects": deque(user + ["has_ar_effects"]),
-            "has_clips": deque(user + ["has_clips"]),
-            "has_guides": deque(user + ["has_guides"]),
-            "has_channel": deque(user + ["has_channel"]),
-            "has_blocked_viewer": deque(user + ["has_blocked_viewer"]),
-            "highlight_reel_count": deque(user + ["highlight_reel_count"]),
-            "has_requested_viewer": deque(user + ["has_requested_viewer"]),
-            "id": deque(user + ["id"]),
-            "is_business_account": deque(user + ["is_business_account"]),
-            "is_joined_recently": deque(user + ["is_joined_recently"]),
-            "business_category_name": deque(user + ["business_category_name"]),
-            "overall_category_name": deque(user + ["overall_category_name"]),
-            "category_enum": deque(user + ["category_enum"]),
-            "is_private": deque(user + ["is_private"]),
-            "is_verified": deque(user + ["is_verified"]),
-            "mutual_followers": deque(user + ["edge_mutual_followed_by", "count"]),
-            "profile_pic_url": deque(user + ["profile_pic_url"]),
-            "profile_pic_url_hd": deque(user + ["profile_pic_url_hd"]),
-            "requested_by_viewer": deque(user + ["requested_by_viewer"]),
-            "username": deque(user + ["username"]),
-            "connected_fb_page": deque(user + ["connected_fb_page"]),
-            "posts": deque(user + ["edge_owner_to_timeline_media", "count"]),
+            "logging_page_id": deque(["logging_page_id"]),
+            "show_suggested_profiles": deque(["show_suggested_profiles"]),
+            "show_follow_dialog": deque(["show_follow_dialog"]),
+            "biography": deque(["biography"]),
+            "blocked_by_viewer": deque(["blocked_by_viewer"]),
+            "business_email": deque(["business_email"]),
+            "restricted_by_viewer": deque(["restricted_by_viewer"]),
+            "country_block": deque(["country_block"]),
+            "external_url": deque(["external_url"]),
+            "external_url_linkshimmed": deque(["external_url_linkshimmed"]),
+            "followers": deque(["count"]),
+            "followed_by_viewer": deque(["followed_by_viewer"]),
+            "following": deque(["edge_follow_count"]),
+            "follows_viewer": deque(["follows_viewer"]),
+            "full_name": deque(["full_name"]),
+            "has_ar_effects": deque(["has_ar_effects"]),
+            "has_clips": deque(["has_clips"]),
+            "has_guides": deque(["has_guides"]),
+            "has_channel": deque(["has_channel"]),
+            "has_blocked_viewer": deque(["has_blocked_viewer"]),
+            "highlight_reel_count": deque(["highlight_reel_count"]),
+            "has_requested_viewer": deque(["has_requested_viewer"]),
+            "id": deque(["id"]),
+            "is_business_account": deque(["is_business_account"]),
+            "is_joined_recently": deque(["is_joined_recently"]),
+            "business_category_name": deque(["business_category_name"]),
+            "overall_category_name": deque(["overall_category_name"]),
+            "category_enum": deque(["category_enum"]),
+            "is_private": deque(["is_private"]),
+            "is_verified": deque(["is_verified"]),
+            "mutual_followers": deque(["edge_mutual_followed_by_count"]),
+            "profile_pic_url": deque(["profile_pic_url"]),
+            "profile_pic_url_hd": deque(["profile_pic_url_hd"]),
+            "requested_by_viewer": deque(["requested_by_viewer"]),
+            "username": deque(["username"]),
+            "connected_fb_page": deque(["connected_fb_page"]),
+            "posts": deque(["edge_owner_to_timeline_media_count"]),
         }
     )
-
 
 class _HashtagMapping(_GeneralMapping):
     """Mapping specific to Instagram hashtag pages"""
@@ -208,12 +204,10 @@ class _HashtagMapping(_GeneralMapping):
         }
     )
 
-
 class _LoginMapping(_GeneralMapping):
     """Mapping specific to Instagram login page"""
 
     mapping = _GeneralMapping.return_mapping().copy()
-
 
 class _MetaMapping:
     """
