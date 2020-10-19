69fe67c5a420e7b9086fcee29187c9f00e75f1a1
chris-greening <chrisgreening3@gmail.com>
2020-10-19 00:53:31 -0400

    Added docstrings and type hints

--- instascrape/core/_json_flattener.py
+++ instascrape/core/_json_flattener.py
@@ -1,21 +1,25 @@
 from collections import deque
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Union
 from copy import deepcopy
 
 JSONDict = Dict[str, Any]
 
 class JsonTree:
-    """Build a tree of linked lists that map out the JSON data"""
-    def __init__(self, json_dict):
+    """Tree of linked lists that map out the JSON data"""
+    def __init__(self, json_dict: JSONDict):
         self.json_dict = json_dict 
         self.map_tree(self.json_dict)
 
     def map_tree(self, json_dict):
+        """Map the entire JSON tree and get access to leaf nodes"""
         self.leaf_nodes = []
         self.root_node = Node(json_data=json_dict, tree=self)
 
 class Node:
-    def __init__(self, json_data, tree, linked_list=deque([]), prior_keys=[]):
+    """
+    Representation of one step into a JSON Tree
+    """
+    def __init__(self, json_data: Any, tree: JsonTree, linked_list: deque=deque([]), prior_keys: List[Union[str, int]]=[]) -> None:
         self.json_data = json_data
         self.tree = tree
         self.linked_list = linked_list
@@ -34,10 +38,16 @@ class Node:
 
     @property
     def is_leaf(self):
+        """
+        If the dtype of self.json_data is not a dict or a list then it must be
+        a leaf node
+        """
         return self.dtype is not list and self.dtype is not dict
 
     def get_edges(self):
-        #If dict, only iterate over the values
+        """
+        Get all edges connected to current Node
+        """
         iter_arr = zip(range(len(self.json_data)), self.json_data) if self.dtype is list else self.json_data.items()
 
         for key, value in iter_arr:
