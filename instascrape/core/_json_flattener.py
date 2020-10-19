313010c7b614c89f09f8300c08b06dc080bb97d8
chris-greening <chrisgreening3@gmail.com>
2020-10-19 00:46:35 -0400

    Rewrote Node class, added inst ref to Tree so Tree can track leaf nodes

--- instascrape/core/_json_flattener.py
+++ instascrape/core/_json_flattener.py
@@ -1,51 +1,50 @@
+from collections import deque
 from typing import List, Dict, Any
 from copy import deepcopy
 
 JSONDict = Dict[str, Any]
 
 class JsonTree:
-    """Flatten a nested dictionary of JSON-data as much as possible"""
+    """Build a tree of linked lists that map out the JSON data"""
     def __init__(self, json_dict):
         self.json_dict = json_dict 
         self.map_tree(self.json_dict)
 
     def map_tree(self, json_dict):
-        self.root_node = Node(json_data=json_dict)
+        self.leaf_nodes = []
+        self.root_node = Node(json_data=json_dict, tree=self)
 
 class Node:
-    leaf_nodes = []
-    def __init__(self, json_data: JSONDict, linked_list: List['Node'] = [], prior_key: str = ''):
+    def __init__(self, json_data, tree, linked_list=deque([]), prior_keys=[]):
         self.json_data = json_data
+        self.tree = tree
         self.linked_list = linked_list
+        self.prior_keys = prior_keys
 
         self.dtype = type(self.json_data)
-        self.next_linked_list = self.linked_list + [self]
-        self.edges = []
+
+        self.nodes = []
 
         if self.is_leaf:
-            self.json_data = {prior_key: self.json_data}
-            Node.append_leaf_node(self)
+            self.json_data = {prior_keys[-1]: self.json_data}
+            self.tree.leaf_nodes.append(self)
+
         else:
             self.get_edges()
 
     @property
     def is_leaf(self):
-        """
-        If the type of self.json_data is not a list or dict, then this Node
-        must be a leaf
-        """
         return self.dtype is not list and self.dtype is not dict
 
     def get_edges(self):
-        if type(self.json_data) is list:
-            for i, next_step in enumerate(self.json_data):
-                node = Node(next_step, self.next_linked_list, i)
-                self.edges.append(node)
-        else:
-            for key, next_step in self.json_data.items():
-                node = Node(next_step, self.next_linked_list, key)
-                self.edges.append(node)
+        #If dict, only iterate over the values
+        iter_arr = zip(range(len(self.json_data)), self.json_data) if self.dtype is list else self.json_data.items()
 
-    @classmethod
-    def append_leaf_node(cls, node):
-        cls.leaf_nodes.append(node)
+        for key, value in iter_arr:
+            next_linked_list = self.linked_list + deque([self])
+            next_key = self.prior_keys + [key]
+            node = Node(value, self.tree, next_linked_list, next_key)
+            self.nodes.append(node)
+
+    def __repr__(self):
+        return str(self.json_data)
