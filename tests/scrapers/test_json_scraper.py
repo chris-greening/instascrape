5ae44b884cb788dd7e813f24dca9ce5707fb1bc2
chris-greening <chrisgreening3@gmail.com>
2020-10-19 22:45:51 -0400

    Changed to reflect new implementation

--- tests/scrapers/test_json_scraper.py
+++ tests/scrapers/test_json_scraper.py
@@ -2,34 +2,27 @@ import pytest
 
 import requests
 
-from instascrape import JsonScraper
+from instascrape.scrapers.json_tools import json_from_html, determine_json_type, json_from_url
 
+@pytest.fixture
+def source_html():
+    """Return the source HTML from the given page"""
+    url = "https://www.instagram.com/chris_greening/"
+    source_html = requests.get(url).text
+    return source_html
 
-class TestJsonScraper:
-    @pytest.fixture
-    def json_scraper(self):
-        """Return instance of JsonScraper"""
-        return JsonScraper()
+def test_json_from_html(source_html):
+    json_dict = json_from_html(source_html)
+    assert type(json_dict) == dict
+    assert "entry_data" in json_dict
 
-    @pytest.fixture
-    def source_html(self):
-        """Return the source HTML from the given page"""
-        url = "https://www.instagram.com/chris_greening/"
-        source_html = requests.get(url).text
-        return source_html
+def test_determine_json_type(source_html):
+    json_dict = json_from_html(source_html)
+    json_type = determine_json_type(json_dict)
+    assert json_type == "ProfilePage"
 
-    def test_json_from_html(self, json_scraper, source_html):
-        json_dict = json_scraper.json_from_html(source_html)
-        assert type(json_dict) == dict
-        assert "entry_data" in json_dict
-
-    def test_determine_json_type(self, json_scraper, source_html):
-        json_dict = json_scraper.json_from_html(source_html)
-        json_type = json_scraper.determine_json_type(json_dict)
-        assert json_type == "ProfilePage"
-
-    def test_json_from_url(self, json_scraper):
-        url = "https://www.instagram.com/chris_greening/"
-        json_dict = json_scraper.json_from_url(url)
-        assert type(json_dict) == dict
-        assert "entry_data" in json_dict
+def test_json_from_url():
+    url = "https://www.instagram.com/chris_greening/"
+    json_dict = json_from_url(url)
+    assert type(json_dict) == dict
+    assert "entry_data" in json_dict
