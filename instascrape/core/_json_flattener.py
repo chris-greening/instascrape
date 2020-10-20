0892374019f48fef671cf027844816eee35a3a1c
chris-greening <chrisgreening3@gmail.com>
2020-10-19 23:14:41 -0400

    refactor: removed unnecessary complexity from _flatten_json

--- instascrape/core/_json_flattener.py
+++ instascrape/core/_json_flattener.py
@@ -18,20 +18,23 @@ class FlatJSONDict(MutableMapping):
         self.__dict__.update(self.flat_json)
 
     def _flatten_json(self):
+        """
+        Primary algorithm that creates the flattened dictionary from a mapped
+        tree of JSON data
+        """
         flattened_dict = {}
         for leaf_node in self.json_tree.leaf_nodes:
             key_arr = deque([])
-            for key in leaf_node.json_data.keys():
-                key_arr.appendleft(str(key))  # ensure key is str
-                new_key = '_'.join(key_arr)
-                if new_key in flattened_dict:
-                    for prior_key in leaf_node.prior_keys[-2::-1]:
-                        key_arr.appendleft(str(prior_key))
-                        new_key = '_'.join(key_arr)
-                        if new_key not in flattened_dict:
-                            break
-                flattened_dict[new_key] = list(leaf_node.json_data.values())[0]
+            for key in leaf_node.prior_keys[::-1]:
+                new_key = self._new_key(key, key_arr)
+                if new_key not in flattened_dict:
+                    break
+            flattened_dict[new_key] = list(leaf_node.json_data.values())[0]
         return flattened_dict
+
+    def _new_key(self, key: str, key_arr: deque) -> str:
+        key_arr.appendleft(str(key))
+        return '_'.join(key_arr)
 
     def __setitem__(self, key, value):
         self.__dict__[key] = value
