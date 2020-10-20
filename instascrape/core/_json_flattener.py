48ac1d62e0244e9047369125515ecd26894f5cd4
chris-greening <chrisgreening3@gmail.com>
2020-10-19 21:12:59 -0400

    Reimplemented JSON flattener as FlatJSONDict

--- instascrape/core/_json_flattener.py
+++ instascrape/core/_json_flattener.py
@@ -1,24 +1,28 @@
 from collections import deque
+from collections.abc import MutableMapping
 from typing import List, Dict, Any, Union
 from copy import deepcopy
 
 JSONDict = Dict[str, Any]
 
-class JsonFlattener:
-    """Flatten an input JSON dictionary to keys based on leaf node key names"""
-    def __init__(self, json_dict: JSONDict):
+class FlatJSONDict(MutableMapping):
+    def __init__(self, json_dict):
+        """Takes a dictionary with JSON-like data and creates an instance that
+        behaves exactly like a dict but with the flattened data"""
         self.json_dict = json_dict
 
         self.json_tree = JsonTree(self.json_dict)
 
-        self.flat_json = self.flatten_json()
+        self.flat_json = self._flatten_json()
 
-    def flatten_json(self):
+        self.__dict__.update(self.flat_json)
+
+    def _flatten_json(self):
         flattened_dict = {}
         for leaf_node in self.json_tree.leaf_nodes:
             key_arr = deque([])
             for key in leaf_node.json_data.keys():
-                key_arr.appendleft(str(key))        #ensure key is str
+                key_arr.appendleft(str(key))  # ensure key is str
                 new_key = '_'.join(key_arr)
                 if new_key in flattened_dict:
                     for prior_key in leaf_node.prior_keys[-2::-1]:
@@ -28,6 +32,32 @@ class JsonFlattener:
                             break
                 flattened_dict[new_key] = list(leaf_node.json_data.values())[0]
         return flattened_dict
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __delitem__(self, key):
+        del self.__dict__[key]
+
+    def __iter__(self):
+        return iter(self.__dict__)
+
+    def __len__(self):
+        return len(self.__dict__)
+    # The final two methods aren't required, but nice for demo purposes:
+
+    def __str__(self):
+        '''returns simple dict representation of the mapping'''
+        return str(self.__dict__)
+
+    def __repr__(self):
+        '''echoes class, id, & reproducible representation in the REPL'''
+        return '{}, D({})'.format(super(FlatJSONDict, self).__repr__(),
+                                  self.__dict__)
+
 
 class JsonTree:
     """Tree of linked lists that map out the JSON data"""
