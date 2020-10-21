c16095a64bcf473ba2dfde3cc70373dae8a77034
chris-greening <chrisgreening3@gmail.com>
2020-10-21 19:42:15 -0400

    Incr minor number 0.6.7 -> 0.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="insta-scrape",
-    version="0.6.7",
+    version="0.7.0",
     author="Chris Greening",
     author_email="chris@christophergreening.com",
     description="Super lightweight Instagram web scraper for data analysis",
