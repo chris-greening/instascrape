0bba87224811a3b7e2ae4801f8a0fdc04d9c5378
chris-greening <chrisgreening3@gmail.com>
2020-10-26 15:38:32 -0400

    docs: Added docstrings to class and methods

--- instascrape/instascrape.py
+++ instascrape/instascrape.py
@@ -4,17 +4,63 @@ from instascrape.scrapers import *
 
 
 class InstaScrape:
+    """
+    Quick and clean interface for tying in much of instascrape's functionality
+    in easy and clean syntax
+    """
     def load_profile(self, username: str) -> Profile:
+        """
+        Load, scrape, and return a Profile object that contains all of the
+        availably scraped data from a given profile
+
+        Parameters
+        ----------
+        username : str
+            Username of the profile to be scraped
+
+        Returns
+        -------
+        profile : Profile
+            Profile object with the scraped data
+        """
         profile = Profile.from_username(username)
         profile.load()
         return profile
 
     def load_post(self, url: str) -> Post:
+        """
+        Load, scrape, and return a Post object that contains all of the
+        availably scraped data from a given post
+
+        Parameters
+        ----------
+        url : str
+            Full URL of the post to be scraped
+
+        Returns
+        -------
+        post : Post
+            Post object with the scraped data
+        """
         post = Post(url)
         post.load()
         return post
 
     def load_hashtag(self, hashtag: str) -> Hashtag:
+        """
+        Load, scrape, and return a Hashtag object that contains all of the
+        availably scraped data from a given hashtag
+
+        Parameters
+        ----------
+        hashtag : str
+            Name of the hashtag to be scraped
+
+        Returns
+        -------
+        hashtag : Hashtag
+            Hashtag object with the scraped data
+        """
         hashtag = Hashtag.from_hashtag(hashtag)
         hashtag.load()
         return hashtag
