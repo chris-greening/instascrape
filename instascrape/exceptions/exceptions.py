fc2b0ac01ec4cc0c59d39968b7a658eb87bd3447
chris-greening <chrisgreening3@gmail.com>
2020-11-09 19:25:48 -0500

    refactor: changed name to InstagramLoginRedirectError

--- instascrape/exceptions/exceptions.py
+++ instascrape/exceptions/exceptions.py
@@ -1,3 +1,3 @@
-class InstagramLoginRedirect(Exception):
+class InstagramLoginRedirectError(Exception):
     def __init__(self, message="Instagram is redirecting you to the login page. Login to Instagram on your machine and try again"):
         super().__init__(message)
