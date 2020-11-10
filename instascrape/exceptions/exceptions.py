878b1e5a5acce424a6537399d6e116645d4a69be
chris-greening <chrisgreening3@gmail.com>
2020-11-09 21:21:36 -0500

    add: added WrongSourceError exception

--- instascrape/exceptions/exceptions.py
+++ instascrape/exceptions/exceptions.py
@@ -1,3 +1,7 @@
 class InstagramLoginRedirectError(Exception):
     def __init__(self, message="Instagram is redirecting you to the login page. Login to Instagram on your machine and try again"):
         super().__init__(message)
+
+class WrongSourceError(Exception):
+    def __init__(self, message="Wrong input source, use the correct class"):
+        super().__init__(message)
