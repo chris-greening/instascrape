bf04ad65f991febc2c197c38d3ebc64e9eea518a
chris-greening <chrisgreening3@gmail.com>
2020-11-09 23:23:02 -0500

    tests: added to_json and to_csv tests

--- tests/scrapers/test_post.py
+++ tests/scrapers/test_post.py
@@ -41,40 +41,24 @@ class TestPost:
     def test_to_dict(self, page_instance):
         assert isinstance(page_instance.to_dict(), dict)
 
-    # @pytest.mark.file_io
-    # def test_to_json(self, page_instance, tmpdir):
-    #     file = tmpdir.join("data.json")
-    #     page_instance.to_json(fp=str(file))
-    #     with open(str(file), "r") as injson:
-    #         json_dict = json.load(injson)
-    #     json_dict["upload_date"] = datetime.datetime.fromtimestamp(int(json_dict["upload_date"]))
+    @pytest.mark.file_io
+    def test_to_json(self, page_instance, tmpdir):
+        file = tmpdir.join("data.json")
+        page_instance.to_json(fp=str(file))
+        with open(str(file), "r") as injson:
+            json_dict = json.load(injson)
+        assert page_instance['shortcode'] == json_dict['shortcode']
 
-    #     # NaN will fail on nan == nan so convert them to 'nan'
-    #     page_inst_dict = page_instance.to_dict()
-    #     page_inst_dict = {key: str(val) for key, val in page_inst_dict.items()}
-    #     json_dict = {key: str(val) for key, val in page_inst_dict.items()}
+    @pytest.mark.file_io
+    def test_to_csv(self, page_instance, tmpdir):
 
-    #     assert page_inst_dict == json_dict
+        # write to CSV
+        file = tmpdir.join("data.csv")
+        page_instance.to_csv(fp=str(file))
 
-    # @pytest.mark.file_io
-    # def test_to_csv(self, page_instance, tmpdir):
+        # reread the csv
+        with open(str(file), mode="r") as infile:
+            reader = csv.reader(infile)
+            csv_dict = {row[0]: row[1] for row in reader}
 
-    #     # write to CSV
-    #     file = tmpdir.join("data.csv")
-    #     page_instance.to_csv(fp=str(file))
-
-    #     # reread the csv
-    #     with open(str(file), mode="r") as infile:
-    #         reader = csv.reader(infile)
-    #         csv_dict = {row[0]: row[1] for row in reader}
-    #     csv_dict["upload_date"] = str(datetime.datetime.fromtimestamp(float(csv_dict["upload_date"])))
-
-    #     # have to convert everything to str otherwise AssertionError will trip
-    #     # up comparing stuff like True == 'True'
-    #     str_dict = {}
-    #     for key, val in page_instance.to_dict().items():
-    #         if val is None:
-    #             val = ""
-    #         str_dict[key] = str(val)
-
-    #     assert str_dict == csv_dict
+        assert page_instance['shortcode'] == csv_dict['shortcode']
