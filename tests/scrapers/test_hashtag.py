d468e47ca94d57edb71a02c4bb97e0e02eb2de16
chris-greening <chrisgreening3@gmail.com>
2020-11-09 23:19:11 -0500

    tests: added to_json and to_csv tests

--- tests/scrapers/test_hashtag.py
+++ tests/scrapers/test_hashtag.py
@@ -40,32 +40,24 @@ class TestHashtag:
     def test_to_dict(self, page_instance):
         assert isinstance(page_instance.to_dict(), dict)
 
-    # @pytest.mark.file_io
-    # def test_to_json(self, page_instance, tmpdir):
-    #     file = tmpdir.join("data.json")
-    #     page_instance.to_json(fp=str(file))
-    #     with open(str(file), "r") as injson:
-    #         json_dict = json.load(injson)
-    #     assert page_instance.to_dict() == json_dict
+    @pytest.mark.file_io
+    def test_to_json(self, page_instance, tmpdir):
+        file = tmpdir.join("data.json")
+        page_instance.to_json(fp=str(file))
+        with open(str(file), "r") as injson:
+            json_dict = json.load(injson)
+        assert page_instance['name'] == json_dict['name']
 
-    # @pytest.mark.file_io
-    # def test_to_csv(self, page_instance, tmpdir):
+    @pytest.mark.file_io
+    def test_to_csv(self, page_instance, tmpdir):
 
-    #     # write to CSV
-    #     file = tmpdir.join("data.csv")
-    #     page_instance.to_csv(fp=str(file))
+        # write to CSV
+        file = tmpdir.join("data.csv")
+        page_instance.to_csv(fp=str(file))
 
-    #     # reread the csv
-    #     with open(str(file), mode="r") as infile:
-    #         reader = csv.reader(infile)
-    #         csv_dict = {row[0]: row[1] for row in reader}
+        # reread the csv
+        with open(str(file), mode="r") as infile:
+            reader = csv.reader(infile)
+            csv_dict = {row[0]: row[1] for row in reader}
 
-    #     # have to convert everything to str otherwise AssertionError will trip
-    #     # up comparing stuff like True == 'True'
-    #     str_dict = {}
-    #     for key, val in page_instance.to_dict().items():
-    #         if val is None:
-    #             val = ""
-    #         str_dict[key] = str(val)
-
-    #     assert str_dict == csv_dict
+        assert page_instance['name'] == csv_dict['name']
