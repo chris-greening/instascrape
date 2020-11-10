c0097206a4797081a4ea45e649fa7cddcec5b730
chris-greening <chrisgreening3@gmail.com>
2020-11-09 22:54:53 -0500

    docs: added docstrings to exceptions

--- instascrape/exceptions/exceptions.py
+++ instascrape/exceptions/exceptions.py
@@ -1,7 +1,15 @@
 class InstagramLoginRedirectError(Exception):
+    """
+    Exception that indicates Instagram is redirecting away from the page
+    that should be getting scraped. Can be remedied by logging into Instagram.
+    """
     def __init__(self, message="Instagram is redirecting you to the login page. Login to Instagram on your machine and try again"):
         super().__init__(message)
 
 class WrongSourceError(Exception):
+    """
+    Exception that indicates user passed the wrong source type to the scraper.
+    An example is passing a URL of a hashtag to a Profile object
+    """
     def __init__(self, message="Wrong input source, use the correct class"):
         super().__init__(message)
