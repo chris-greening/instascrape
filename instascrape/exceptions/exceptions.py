9f2a85da33a8b29f59c143be128d8223f698f26e
chris-greening <chrisgreening3@gmail.com>
2020-11-09 19:24:32 -0500

    refactor: rewrote login redirect message

--- instascrape/exceptions/exceptions.py
+++ instascrape/exceptions/exceptions.py
@@ -1,3 +1,3 @@
 class InstagramLoginRedirect(Exception):
-    def __init__(self, message="You do not have valid cookies. Login to Instagram on your machine and try again"):
+    def __init__(self, message="Instagram is redirecting you to the login page. Login to Instagram on your machine and try again"):
         super().__init__(message)
