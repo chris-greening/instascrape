8d014edb60fd09179286a44683f9ff75ecab88ba
chris-greening <chrisgreening3@gmail.com>
2020-11-10 21:11:54 -0500

    version: incr patch version number 1.2.1 -> 1.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="insta-scrape",
-    version="1.2.1",
+    version="1.2.2",
     author="Chris Greening",
     author_email="chris@christophergreening.com",
     description="Super lightweight Instagram web scraper for data analysis",
