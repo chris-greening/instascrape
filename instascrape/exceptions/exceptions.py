502d8fea2503d815130a74cedd3c8a9e2da494d1
chris-greening <chrisgreening3@gmail.com>
2020-11-23 20:17:43 -0500

    modify: chhanged InstagramLoginRedirectError description to be more descriptive

--- instascrape/exceptions/exceptions.py
+++ instascrape/exceptions/exceptions.py
@@ -3,7 +3,7 @@ class InstagramLoginRedirectError(Exception):
     Exception that indicates Instagram is redirecting away from the page
     that should be getting scraped. Can be remedied by logging into Instagram.
     """
-    def __init__(self, message="Instagram is redirecting you to the login page. Login to Instagram on your machine and try again"):
+    def __init__(self, message="Instagram is redirecting you to the login page instead of the page you are trying to scrape. This could be occuring because you 1. made too many requests too quickly or 2. are not logged into Instagram on your machine."):
         super().__init__(message)
 
 class WrongSourceError(Exception):
