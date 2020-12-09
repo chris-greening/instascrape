50f32f3aada6165d510fc2eb6695ca3fc75ff5cd
chris-greening <chrisgreening3@gmail.com>
2020-12-08 19:39:07 -0500

    modify: incr version number 1.3.3 -> 1.3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="insta-scrape",
-    version="1.3.3",
+    version="1.3.4",
     author="Chris Greening",
     author_email="chris@christophergreening.com",
     description="Super lightweight Instagram web scraper for data analysis",
