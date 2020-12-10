b6e06aba65cbe4a4c41cef5a56524cd1dce4e61d
chris-greening <chrisgreening3@gmail.com>
2020-12-10 17:09:23 -0500

    add: added get top posts method for Location

--- instascrape/scrapers/location.py
+++ instascrape/scrapers/location.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import List
 
-from instascrape.core._mappings import _LocationMapping
+from instascrape.core._mappings import _LocationMapping, _PostMapping
 from instascrape.core._static_scraper import _StaticHtmlScraper
 from instascrape.scrapers.post import Post
 
@@ -11,39 +11,32 @@ class Location(_StaticHtmlScraper):
 
     _Mapping = _LocationMapping
 
-    # def get_recent_posts(self, amt: int = 12) -> List[Post]:
-    #     """
-    #     Return a list of the profiles recent posts
+    def get_top_posts(self, amt: int = 71) -> List[Post]:
+        """
+        Return a list of recent posts to the hasthag
 
-    #     Parameters
-    #     ----------
-    #     amt : int
-    #         Amount of recent posts to return
+        Parameters
+        ----------
+        amt : int
+            Amount of recent posts to return
 
-    #     Returns
-    #     -------
-    #     posts : List[Post]
-    #         List containing the recent 12 posts and their available data
-    #     """
-    #     if amt > 12:
-    #         raise IndexError(
-    #             f"{amt} is too large, 12 is max available posts. Getting more posts will require an out-of-the-box extension.")
-    #     posts = []
-    #     try:
-    #         post_arr = self.json_dict["entry_data"]["ProfilePage"][0]["graphql"]["user"]["edge_owner_to_timeline_media"][
-    #             "edges"
-    #         ]
-    #     except TypeError:
-    #         raise ValueError(
-    #             "Can't return posts without first scraping the Profile. Call the scrape method on your object first.")
-
-    #     for post in post_arr[:amt]:
-    #         json_dict = post["node"]
-    #         mapping = _PostMapping.post_from_profile_mapping()
-    #         post = Post(json_dict)
-    #         post.scrape(mapping=mapping)
-    #         posts.append(post)
-    #     return posts
+        Returns
+        -------
+        posts : List[Post]
+            List containing the recent 12 posts and their available data
+        """
+        posts = []
+        post_arr = self.json_dict["entry_data"]["LocationsPage"][0]["graphql"]["location"]["edge_location_to_media"]["edges"]
+        amount_of_posts = len(post_arr)
+        if amt > amount_of_posts:
+            amt = amount_of_posts
+        for post in post_arr[:amt]:
+            json_dict = post["node"]
+            mapping = _PostMapping.post_from_hashtag_mapping()
+            post = Post(json_dict)
+            post.scrape(mapping=mapping)
+            posts.append(post)
+        return posts
 
     def _url_from_suburl(self, suburl):
         raise ValueError("suburl not currently supported for Location scrape, please pass a full URL to the location you want to scrape")
