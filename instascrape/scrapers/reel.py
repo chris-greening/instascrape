cd2fe6e870df0a9c7ebedabc6bd434e1c52809f6
chris-greening <chrisgreening3@gmail.com>
2020-12-13 22:35:14 -0500

    docs: added class docstring

--- instascrape/scrapers/reel.py
+++ instascrape/scrapers/reel.py
@@ -2,6 +2,8 @@ from instascrape.scrapers.post import Post
 from instascrape.core._mappings import _ReelMapping
 
 class Reel(Post):
+    """Scraper for an Instagram reel"""
+    
     _Mapping = _ReelMapping
 
     @staticmethod
