1976c68d53912cb2555042df31f5f832ab741f2d
chris-greening <chrisgreening3@gmail.com>
2021-01-15 22:13:46 -0500

    remove: removed import of json_tools

--- instascrape/scrapers/__init__.py
+++ instascrape/scrapers/__init__.py
@@ -3,7 +3,6 @@ Primary API scraper tools
 """
 
 from instascrape.scrapers.hashtag import *
-from instascrape.scrapers.json_tools import *
 from instascrape.scrapers.login import *
 from instascrape.scrapers.post import *
 from instascrape.scrapers.profile import *
