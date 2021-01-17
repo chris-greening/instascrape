7582280edbbb40494e99240aa9196876f963fbd1
chris-greening <chrisgreening3@gmail.com>
2021-01-16 23:25:37 -0500

    fix: removed imports that don't exist anymore

--- instascrape/scrapers/__init__.py
+++ instascrape/scrapers/__init__.py
@@ -3,7 +3,6 @@ Primary API scraper tools
 """
 
 from instascrape.scrapers.hashtag import *
-from instascrape.scrapers.login import *
 from instascrape.scrapers.post import *
 from instascrape.scrapers.profile import *
 from instascrape.scrapers.comment import *
